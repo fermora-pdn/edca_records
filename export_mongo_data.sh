4a1d0454180e783840bfab68eeeb1a52dff11d90
Mario Torres <mario291187@gmail.com>
2017-11-15 17:05:39 -0600

    fix: export_mongo.data.sh

--- export_mongo_data.sh
+++ export_mongo_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mongoexport -d buda -c contratacionesabiertas --out contratacionesabiertas_bulk.json --jsonArray
-mongoexport -d buda -c Records --out contratacionesabiertas_bulk.csv --type=csv --fieldFile ../fieldFile.txt
+mongoexport -d buda -c Records --out contratacionesabiertas_bulk.csv --type=csv --fieldFile fieldFile.txt
  
